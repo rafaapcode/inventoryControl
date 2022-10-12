7ae9f0b9113df179fdf8aa0ffd0635030d869bc4
rafaapcode <rafaap2003@gmail.com>
2022-10-12 09:36:10 -0300

    Error Handling

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -2,33 +2,61 @@ import Product from '../models/Product.js';
 
 export default class ProductController {
   static async createProduct(req, res) {
-    const newProduct = await Product.createProduct(req.body);
+    try {
+      const newProduct = await Product.createProduct(req.body);
 
-    return res.json(newProduct);
+      return res.json(newProduct);
+    } catch (error) {
+      res.staus(500).json({ error: 'Errot to create a product' });
+    }
   }
 
   static async getProduct(req, res) {
-    const { name } = req.params;
-    const product = await Product.getProduct(name);
+    try {
+      const { name } = req.params;
+      const product = await Product.getProduct(name);
 
-    return res.json(product);
+      if (!product) {
+        res.json({ error: 'Product not found' });
+      }
+
+      return res.json(product);
+    } catch (error) {
+      res.staus(404).json(null);
+    }
   }
 
   static async getAllProducts(req, res) {
-    const products = await Product.allProducts();
+    try {
+      const products = await Product.allProducts();
 
-    return res.json(products);
+      if (!products) {
+        res.json(null);
+      }
+
+      return res.json(products);
+    } catch (error) {
+      res.staus(404).json(null);
+    }
   }
 
   static async updateProducts(req, res) {
-    await Product.updateProduct(req.params.name, req.body);
+    try {
+      await Product.updateProduct(req.params.name, req.body);
 
-    return res.json({ message: 'Product updated successfully.' });
+      return res.json({ message: 'Product updated successfully.' });
+    } catch (error) {
+      res.staus(404).json({ error: 'Product not found' });
+    }
   }
 
   static async deleteProducts(req, res) {
-    await Product.deleteProduct(req.params.name);
+    try {
+      await Product.deleteProduct(req.params.name);
 
-    return res.json({ message: 'Product deleted successfully.' });
+      return res.json({ message: 'Product deleted successfully.' });
+    } catch (error) {
+      res.staus(404).json({ error: 'Product not found' });
+    }
   }
 }
--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,33 +3,41 @@ import User from '../models/User.js';
 
 class UserController {
   async createUser(req, res) {
-    const validatorEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    try {
+      const validatorEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if (!validatorEmail.test(req.body.email) || req.body.password < 5) {
-      return res.status(401).json({
-        message: ['Password must be at least 7 characters', 'Email incorrect'],
-      });
+      if (!validatorEmail.test(req.body.email) || req.body.password < 5) {
+        return res.status(401).json({
+          message: ['Password must be at least 7 characters', 'Email incorrect'],
+        });
+      }
+
+      if (await (User.getUser(req.body.email))) {
+        return res.status(401).json({
+          message: ['User already exists.'],
+        });
+      }
+
+      const salt = bcryptjs.genSaltSync();
+      req.body.password = await bcryptjs.hashSync(req.body.password, salt);
+
+      const newUser = await User.createUser(req.body);
+
+      res.json(newUser);
+    } catch (error) {
+      res.json({ message: 'Error creating a user' });
     }
-
-    if (await (User.getUser(req.body.email))) {
-      return res.status(401).json({
-        message: ['User already exists.'],
-      });
-    }
-
-    const salt = bcryptjs.genSaltSync();
-    req.body.password = await bcryptjs.hashSync(req.body.password, salt);
-
-    const newUser = await User.createUser(req.body);
-
-    res.json(newUser);
   }
 
   async getUser(req, res) {
-    const { email } = req.body;
-    const user = await User.getUser(email);
+    try {
+      const { email } = req.body;
+      const user = await User.getUser(email);
 
-    res.json(user);
+      res.json(user);
+    } catch (error) {
+      res.json({ message: 'User not found' });
+    }
   }
 }
 
